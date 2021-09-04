330693b2ddfd297fc3b1723aa46b6f4b1c171d4c
FrancisCuenca-ECP04 <yahshua.ecpdalet@gmail.com>
2021-09-04 15:50:58 +0800

    added post detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls.resolvers import URLPattern
 from  . import views
 
 urlpatterns = [
-    path('',views.post_list,name='post_list')
+    path('',views.post_list,name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
