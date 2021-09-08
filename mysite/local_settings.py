077391c13c17d6faf1533afb963a6b2b4a27b38f
FrancisCuenca-ECP04 <yahshua.ecpdalet@gmail.com>
2021-09-08 13:31:16 +0800

    addition files and changes for Heroku

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,1 +1,14 @@
 DJAGO_SECRET_KEY="10d4dfd0e671dfe3939996fbb853a054bd6ee65c"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'yahshuafrc',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+DEBUG = True
