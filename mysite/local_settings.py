86efbbe98ecbab77de3a841435372a8193b02316
FrancisCuenca-ECP04 <yahshua.ecpdalet@gmail.com>
2021-09-08 13:51:29 +0800

    local_setting.py changed databases

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,13 +1,12 @@
+import os
+
 DJAGO_SECRET_KEY="10d4dfd0e671dfe3939996fbb853a054bd6ee65c"
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangogirls',
-        'USER': 'yahshuafrc',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
